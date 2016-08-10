c72b06fd9cc2dfba6501c4daeab474db20139697
Dan Jenner <danjenner@gmail.com>
2016-08-10 14:24:48 -0500

    Finally works, allows selection between X and O.  Knows if an X or O is chosen, and which one to switch to.

--- app.js
+++ app.js
@@ -9,39 +9,24 @@ var newObj = Object.create(ticObj);
 
 $(document).ready(function () {
 
-
-    $('div').on("click", "div,img", function (event) {
-        event.preventDefault();
-
-        if (event.target.src = "X.png") {
-            var divID = $('img').parent("div").attr("id");
-            alert(divID);
-            var html = '<img class="' + 'img"' + 'src = ' + newObj.o + '>';
-            divID = '"div.' + divID + '"';
-            alert(event.target);
-            //$(event.target).parent('div').html(html);
-
-            //alert("return a O in DIV: " + event.target.src);
-            //alert(html);
-        } else if (event.target.src = "O.png") {
+    $('div').click(function (event) {
+        event.preventDefault(event);
+        if ($(event.target).is('div')) {
             var html = '<img class="' + 'img"' + 'src = ' + newObj.x + '>';
-            $(this).html(html);
-            alert("return an X");
-        } else {
-            var html = '<img src="">';
-            $(this).html(html);
-            alert("hide the image");
+            var id = 'div' + '.' + event.target.id;
+            $(id).html(html);
+        } else if ($(event.target).is('img')) {
+            if (event.target.src) {
+                if ((event.target.src).includes("X.png")) {
+                    var html = '<img class="' + 'img"' + 'src = ' + newObj.o + '>';
+                    var id = event.currentTarget;
+                    $(id).html(html);
+                } else if ((event.target.src).includes("O.png")) {
+                    var html = '<img class="' + 'img"' + 'src = ' + newObj.x + '>';
+                    var id = event.currentTarget;
+                    $(id).html(html);
+                }
+            }
         }
-
     });
-
-    //    $('div').click(function (event) {
-    //        event.preventDefault();
-    //
-    //        //clicking should cycle through X, O and display:none
-    //        var html = '<img class="' + 'img"' + 'src = ' + newObj.x + '>';
-    //        //var id = 'div' + '.' + event.target.id;
-    //        $(this).html(html);
-    //    });
-
 });
